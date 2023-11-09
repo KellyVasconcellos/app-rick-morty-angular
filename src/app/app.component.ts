972ec864ad7dfb384b4e054178ac8791969d12ed
KellyVasconcellos <kelly.h.vasconcellos@gmail.com>
2023-11-09 14:38:49 -0300

    adjusting api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'app-rick-morty';
-
-  public rickandmorty$!: Observable<any>;
-
-  constructor(private http: HttpClient){}
-
-  ngOnInit(){
-
-    this.http.get("https://rickandmortyapi.com/api").subscribe(d=> console.log(d));
-
-    this.rickandmorty$ = this.http.get("https://rickandmortyapi.com/api")
-  }
 }
